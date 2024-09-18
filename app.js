09b406187eeb6f656b411ff65ca21bfa8b5ff2e4
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-18 13:00:05 -0400

    retrieves the input from the level dropdown

--- app.js
+++ app.js
@@ -18,34 +18,8 @@ for (let i=1; i<=20; i++) {
     levelDropdown.appendChild(levelOption);
 }
 
-// select Max Level for character
-const selectMaxLevel = (selectedValue) => {
-    const selectedMaxLevel = selectedValue.value;
-    chosenMaxLevel.textContent = selectedMaxLevel;
+// select the value for the level max
+const selectMaxLevel = () => {
+    console.log(levelDropdown.value);
 }
 
-const randomizeCharacter = () => {
-
-    // random chosen class
-    const getRandomClass = () => {
-        let randomClassIndex = Math.floor(Math.random() * classList.length)
-        let randomClass = classList[randomClassIndex];
-        
-        let randomClassCapitalized = randomClass[0].toUpperCase() + randomClass.substring(1);
-        return randomClassCapitalized;
-    }
-    
-    // random chosen level
-    const getRandomLevel = (maxLevel) => {
-        let levelCount = 0;
-
-        if (levelCount <= maxLevel) {
-            let randomLevel = Math.floor(Math.random() * maxLevel);
-            return randomLevel;
-        }
-    }
-
-    classInfo.innerHTML = `<p>Your class is ${getRandomClass()} at Level ${getRandomLevel(maxLevel)}<p>`;
-}
-
-randomizeCharacter();
