49102efa34b50634ad028612fedfff67c2392aa9
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-18 09:06:10 -0400

    adds a randomizer to select a class for the character

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 levelDropdown = document.getElementById('dropdown');
 
+// list of available classes
+let classList = ['barbarian', 'bard', 'cleric', 'druid', 'fighter',
+                'monk', 'paladin', 'ranger', 'rogue', 'sorcerer',
+                'warlock', 'wizard', 'artificer'
+]
+
 // create the level dropdown menu
 for (let i=1; i<=20; i++) {
     let levelOption = document.createElement('option');
     levelOption.innerHTML = `${i}`
     levelDropdown.appendChild(levelOption);
 }
+
+// random chosen class
+let randomClassIndex = Math.floor(Math.random() * classList.length)
+let randomClass = classList[randomClassIndex];
+console.log(randomClass);
+
+// random chosen level
