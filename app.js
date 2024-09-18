9ad1f5be3042bc299294829f51e96fa4cf3b70bc
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-18 11:54:28 -0400

    added a response to the dropdown menu
    this will tell the user what max level they chose
    for their character

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 levelDropdown = document.getElementById('dropdown');
+classInfo = document.getElementById('class-info');
+chosenMaxLevel = document.getElementById('chosen-max')
+
+let levelCount = 0;
 
 // list of available classes
 let classList = ['barbarian', 'bard', 'cleric', 'druid', 'fighter',
@@ -9,13 +13,36 @@ let classList = ['barbarian', 'bard', 'cleric', 'druid', 'fighter',
 // create the level dropdown menu
 for (let i=1; i<=20; i++) {
     let levelOption = document.createElement('option');
+    levelOption.value = i;
     levelOption.innerHTML = `${i}`
     levelDropdown.appendChild(levelOption);
 }
 
-// random chosen class
-let randomClassIndex = Math.floor(Math.random() * classList.length)
-let randomClass = classList[randomClassIndex];
-console.log(randomClass);
+// select Max Level for character
+const selectMaxLevel = (selectedValue) => {
+    const maxLevel = selectedValue.value;
+    chosenMaxLevel.textContent = maxLevel;
+}
 
-// random chosen level
+const radomizeCharacter = () => {
+
+    // random chosen class
+    const getRandomClass = () => {
+        let randomClassIndex = Math.floor(Math.random() * classList.length)
+        let randomClass = classList[randomClassIndex];
+        
+        let randomClassCapitalized = randomClass[0].toUpperCase() + randomClass.substring(1);
+        return randomClassCapitalized;
+    }
+    
+    // random chosen level
+    const getRandomLevel = (maxLevel) => {
+        if (levelCount <= maxLevel) {
+            let randomLevel = Math.floor(Math.random() * maxLevel);
+            console.log(randomLevel);
+        }
+    }
+
+    classInfo.innerHTML = `Your class is ${getRandomClass()} at Level`;
+}
+
